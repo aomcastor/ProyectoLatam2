c676f9dc3a65c36d928b26f37c55290397ad49b9
aomcastor <dminventor@hotmail.com>
2023-06-30 03:26:28 -0300

    Modificado header con algunas funcionalidades

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
-// JavaScript source code
+let btn = document.getElementsByClassName("linkNav");
+btn.addEventListener("click", moverBtnArriba);
+btn.addEventListener("mouseleave", moverBtnAbajo);
+
+function moverBtnArriba() {
+    btn.style.color = "red";
+}
+function moverBtnAbajo() {
+    btn.style.top = "0";
+}
